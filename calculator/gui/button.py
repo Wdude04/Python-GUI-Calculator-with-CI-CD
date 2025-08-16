a5806add3c111450d431a02f5e5021abdbe87fe7
Wdude04 <wdude0414@gmail.com>
2025-08-16 03:31:32 -0500

    Wide layout
    Added a wide layout for the calculator that is enabled when the calculator is wider than it is tall.

--- calculator/gui/button.py
+++ calculator/gui/button.py
@@ -11,34 +11,41 @@ class ButtonLayout:
     buttons: list[str]
     size_overrides: dict = field(default_factory=dict)
 
-LAYOUT_STANDARD = ButtonLayout(4, 
+LAYOUT_STANDARD_TALL = ButtonLayout(4, 
                 ["AC",  "CE", "%", "/",
                  "7",   "8",  "9", "x",
                  "4",   "5",  "6", "-",
                  "1",   "2",  "3", "+",
                  "+/-", "0",  ".", "="])
 
-LAYOUT_NO_NEGATE = ButtonLayout(4,
-                ["AC",      "CE", "%", "/",
-                 "7",       "8",  "9", "x",
-                 "4",       "5",  "6", "-",
-                 "1",       "2",  "3", "+",
-                 "0",  ".", "="],
-                 {(2, 4): (2,1)})
-
-LAYOUT_NO_NUMBERS = ButtonLayout(5,
-                ["AC", "CE", "%", ".", "=",
-                 "/", "x", "-", "+",],
-                 {(4,0): (1,2)})
+LAYOUT_STANDARD_WIDE = ButtonLayout(5,
+                ["AC",  "7",   "8",  "9", "/",
+                 "CE",  "4",   "5",  "6", "x",
+                 "%",   "1",   "2",  "3", "-",
+                 "+/-", "0",   ".",  "=", "+"])
 
 def calc_button(root, calculator: api.Calculator, display_text): 
     button = tk.Button(root, text=display_text, command=lambda: calculator.input_button(display_text))
     return button
 
 class CalculatorButtons(AbstractBaseWidget):
+    def __init__(self, calculator):
+        super().__init__(calculator)
+        self.prev_column_count = 0
+
     def _create_layout(self, window_resolution):
         aspect_ratio = window_resolution[0] / window_resolution[1]
-        self.create_buttons(LAYOUT_STANDARD)
+        if aspect_ratio > 1:
+            self.create_buttons(LAYOUT_STANDARD_WIDE)
+            self.prev_column_count = LAYOUT_STANDARD_WIDE.width
+        else:
+            self.create_buttons(LAYOUT_STANDARD_TALL)
+            self.prev_column_count = LAYOUT_STANDARD_TALL.width
+    
+    def _destroy_layout(self):
+        super()._destroy_layout()
+        for i in range(self.prev_column_count):
+            self.frame.columnconfigure(i, weight=0)
     
     def _update(self):
         pass
@@ -58,7 +65,7 @@ class CalculatorButtons(AbstractBaseWidget):
             button.grid(column=x, row=y, sticky=tk.NSEW, columnspan=size_x, rowspan=size_y)
         
         for i in range(layout.width):
-            self.frame.grid_columnconfigure(i, uniform="calc_buttons", weight=1)
+            self.frame.grid_columnconfigure(i, weight=1)
         
         for i in range(int(len(layout.buttons)/layout.width)):
             self.frame.grid_rowconfigure(i, weight=1)
