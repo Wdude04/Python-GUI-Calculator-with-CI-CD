0f01cc7dd699a6252aacd6aeca66f9dbd907d954
Wdude04 <wdude0414@gmail.com>
2025-08-15 23:40:52 -0500

    Update main.py
    Makes the calculator actually able to launch.

--- calculator/main.py
+++ calculator/main.py
@@ -14,6 +14,9 @@ class App:
 
         self.display = display.CalculatorDisplay(self.calculator)
         self.buttons = button.CalculatorButtons(self.calculator)
+
+        self.display.create_gui(self.window)
+        self.buttons.create_gui(self.window)
         
         self.display.frame.pack(fill=tk.BOTH,expand=True,side=tk.TOP,padx=10,pady=(10,0))
         self.buttons.frame.pack(fill=tk.BOTH,expand=True,side=tk.BOTTOM,padx=10,pady=(0,10))
@@ -31,4 +34,5 @@ class App:
 
 if __name__ == "__main__":
     my_app = App()
+    my_app.create_gui()
     my_app.run()
